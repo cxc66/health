410e29c4ffc6f1e60ef47a2d22cbfb445f51b646
cxc66 <420609406@qq.com>
2022-04-29 14:09:01 +0800

    健康饮食

---
+++ app/src/main/java/com/animee/test/HomeMenuActivity.java
@@ -0,0 +1,35 @@
+package com.animee.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.animee.test.food_grid.FoodGridActivity;
+import com.animee.test.food_list.InfoListActivity;
+
+public class HomeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_menu);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.home_btn1:
+                intent.setClass(HomeMenuActivity.this, InfoListActivity.class);
+                break;
+            case R.id.home_btn2:
+                intent.setClass(HomeMenuActivity.this, FoodGridActivity.class);
+                break;
+            case R.id.home_btn3:
+                intent.setClass(HomeMenuActivity.this, AboutActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+}
